1bc007c5c7c77a72582cfc206bfc6104f7975ca4
lukepatterson31 <62485951+lukepatterson31@users.noreply.github.com>
2021-11-09 09:11:00 +0000

    Delete HelloWorld directory

--- HelloWorld/curl-weather-forecast.ps1
+++
@@ -1,8 +0,0 @@
-﻿$StartTime = $(get-date)
-$RequestId = 0
-foreach ($i in 1..5)
-{
-    $RequestId += 1
-    Invoke-WebRequest -Uri "http://localhost:5000/weatherforecast" -UseBasicParsing ;
-    Write-Output ("{0}" -f ($(get-date)-$StartTime))
-}
